074b75bbe1c3855b9b172d0df1462285c559b9f9
logicrius <logicriuscon@gmail.com>
2023-04-03 08:16:30 +0700

    Menambahkan Class GlosarryItem_1302210070

---
+++ modul7_kelompok_eliteglobal/modul7_kelompok_eliteglobal/GlossaryItem_1302210070.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul7_kelompok_eliteglobal
+{
+    internal class GlossaryItem_1302210070
+    {
+        public void ReadJSON()
+        {
+            string jsonString = File.ReadAllText(@"E:\Jurnal_GlobalElite\modul7_kelompok_eliteglobal\modul7_kelompok_eliteglobal\jurnal7_3_130221070.json");
+            dynamic jsonObject = JsonConvert.DeserializeObject<dynamic>(jsonString);
+            dynamic glosent = jsonObject.glossary.GlossDiv.GlossList.GlossEntry;
+
+            Console.WriteLine("\nPrint Gloss Entry");
+            Console.WriteLine(glosent.ID);
+            Console.WriteLine(glosent.SortAs);
+            Console.WriteLine(glosent.GlossTerm);
+            Console.WriteLine(glosent.Acronym);
+            Console.WriteLine(glosent.Abbrev);
+            Console.WriteLine(glosent.GlossDef.para);
+            Console.WriteLine(glosent.GlossDef.GlossSeeAlso[0]);
+            Console.WriteLine(glosent.GlossDef.GlossSeeAlso[1]);
+            Console.WriteLine(glosent.GlossSee);
+        }
+    }
+}
+
--- modul7_kelompok_eliteglobal/modul7_kelompok_eliteglobal/Program.cs
+++ modul7_kelompok_eliteglobal/modul7_kelompok_eliteglobal/Program.cs
@@ -5,3 +5,6 @@ ashar.ReadJSON();
 
 TeamMembers_1302210070 ashar2 = new TeamMembers_1302210070();
 ashar2.ReadJSON();
+
+GlossaryItem_1302210070 ashar3 = new GlossaryItem_1302210070();
+ashar3.ReadJSON();
