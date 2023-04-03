66ced7a4d5c9c3e0f9678bb794b95eb67984ab7a
mghazyfh <126835746+Law1911@users.noreply.github.com>
2023-04-03 07:49:48 +0700

    menambahkan class TeamMembers_1302210106_SteventRangga

--- modul7_kelompok_eliteglobal/modul7_kelompok_eliteglobal/Program.cs
+++ modul7_kelompok_eliteglobal/modul7_kelompok_eliteglobal/Program.cs
@@ -3,3 +3,6 @@ using modul7_kelompok_eliteglobal;
 
 DataMahasiswa_1302210106_SteventRangga a =new DataMahasiswa_1302210106_SteventRangga();
 a.ReadJSON();
+Console.WriteLine("");
+TeamMembers_1302210106_SteventRangga b = new TeamMembers_1302210106_SteventRangga();
+b.ReadJson();
---
+++ modul7_kelompok_eliteglobal/modul7_kelompok_eliteglobal/TeamMembers_1302210106_SteventRangga.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft;
+using Newtonsoft.Json;
+
+namespace modul7_kelompok_eliteglobal
+{
+    internal class TeamMembers_1302210106_SteventRangga
+    {
+        public void ReadJson()
+        {
+            string json = File.ReadAllText(@"E:\Jurnal_GlobalElite\modul7_kelompok_eliteglobal\modul7_kelompok_eliteglobal\jurnal7_2_1302210106.json");
+
+            dynamic js = JsonConvert.DeserializeObject(json);
+
+            Console.WriteLine("Daftar anggota kelompok:");
+            int i = 1;
+            foreach (var members in js.members)
+            {
+                Console.WriteLine($" {i} {members.nim} {members.firstName} {members.lastName} ({members.age}, {members.gender})");
+                i++;
+            }
+        }
+    }
+}
