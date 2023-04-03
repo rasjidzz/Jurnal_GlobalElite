4aa39c2bdb328eec69ff9d6d01fa6df98844d83c
rasjidzz <mrisjads@gmail.com>
2023-04-03 08:11:49 +0700

    menambahkan class GlossaryItem_1302213045

--- modul7_kelompok_eliteglobal/modul7_kelompok_eliteglobal/Program.cs
+++ modul7_kelompok_eliteglobal/modul7_kelompok_eliteglobal/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using modul7_kelompok_eliteglobal;
+
+Console.WriteLine("Soal Jurnal KPL No 3 : ");
+GlossaryItem_1302213045 soal3= new GlossaryItem_1302213045();
+soal3.ReadJSON();
