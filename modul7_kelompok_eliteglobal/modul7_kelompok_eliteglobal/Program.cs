fcc50787e2c92e0142a89f5d96fe5b47ebd3d9c1
ZhafranHafizh <zhafranhafizh@student.telkomuniversity.ac.id>
2023-04-03 07:56:50 +0700

    menambahkan class TeamMembers_1302210036

--- modul7_kelompok_eliteglobal/modul7_kelompok_eliteglobal/Program.cs
+++ modul7_kelompok_eliteglobal/modul7_kelompok_eliteglobal/Program.cs
@@ -3,3 +3,8 @@ using modul7_kelompok_eliteglobal;
 
 DataMahasiswa_1302210036 udin = new DataMahasiswa_1302210036();
 udin.ReadJSON();
+
+Console.WriteLine("");
+
+TeamMembers_1302210036 elit = new TeamMembers_1302210036();
+elit.ReadJSON();
