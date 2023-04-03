9b2aa2a9ee4f62f020fb34e20240bc4bd37f1187
logicrius <logicriuscon@gmail.com>
2023-04-03 07:21:22 +0700

    Menambahkan Class DataMahasiswa_1302210070
    Menambahkan Class DataMahasiswa_1302210070

---
+++ modul7_kelompok_eliteglobal/modul7_kelompok_eliteglobal/DataMahasiswa_1302210070.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace modul7_kelompok_eliteglobal
+{
+    internal class DataMahasiswa_1302210070
+    {
+        public void ReadJSON()
+        {
+            string jsonString = File.ReadAllText(@"E:\Jurnal_GlobalElite\modul7_kelompok_eliteglobal\modul7_kelompok_eliteglobal\jurnal7_1_1302210070.json");
+            dynamic jsonObject = JsonConvert.DeserializeObject<dynamic>(jsonString);
+
+            Console.WriteLine($"Nama: {jsonObject.firstName} {jsonObject.lastName}");
+            Console.WriteLine($"Gender: {jsonObject.gender} dan Umur: {jsonObject.age}");
+            Console.WriteLine($"Address: {jsonObject.address.streetAddress}, {jsonObject.address.city}, {jsonObject.address.state}");
+            Console.WriteLine($"Courses: {jsonObject.courses[0].code}, {jsonObject.courses[0].name}");
+            Console.WriteLine($"Courses: {jsonObject.courses[1].code}, {jsonObject.courses[1].name}");
+        }
+    }
+}
--- modul7_kelompok_eliteglobal/modul7_kelompok_eliteglobal/Program.cs
+++ modul7_kelompok_eliteglobal/modul7_kelompok_eliteglobal/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using modul7_kelompok_eliteglobal;
+
+DataMahasiswa_1302210070 ashar = new DataMahasiswa_1302210070();
+ashar.ReadJSON();
