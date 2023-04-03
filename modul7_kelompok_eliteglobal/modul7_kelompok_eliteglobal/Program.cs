68016eb2974a4e202ef13860972a5c48576fc784
Juliandri04 <juliandrigifari@gmail.com>
2023-04-03 07:43:03 +0700

    menambahkan class TeamMember
    bismillah yaallah

--- modul7_kelompok_eliteglobal/modul7_kelompok_eliteglobal/Program.cs
+++ modul7_kelompok_eliteglobal/modul7_kelompok_eliteglobal/Program.cs
@@ -2,3 +2,6 @@
 
 DataMahasiswa_1302210087_GifariJuliandri x = new DataMahasiswa_1302210087_GifariJuliandri();
 x.ReadJSON();
+
+TeamMember_1302210087_Gifari_Juliandri y = new TeamMember_1302210087_Gifari_Juliandri();
+y.ReadJSON();
---
+++ modul7_kelompok_eliteglobal/modul7_kelompok_eliteglobal/TeamMember_1302210087_Gifari Juliandri.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul7_kelompok_eliteglobal
+{
+    internal class TeamMember_1302210087_Gifari_Juliandri
+    {
+        public void ReadJSON()
+        {
+            string json = File.ReadAllText(@"E:\Jurnal_GlobalElite\modul7_kelompok_eliteglobal\modul7_kelompok_eliteglobal\jurnal7_2_1302210087.json");
+            dynamic jsonGif = JsonConvert.DeserializeObject(json);
+
+            Console.WriteLine("Team Membeer :");
+            int iftar = 1;
+            foreach (var members in jsonGif.members)
+            {
+                Console.WriteLine($" {iftar} {members.nim}-{members.firstName} {members.lastName}, umur: {members.age}, gender: {members.gender}  ");
+                iftar++;
+            }
+        }
+    }
+}
