42cbd0f4653781cbd1b780e9ea19c9adb68897bc
logicrius <logicriuscon@gmail.com>
2023-04-03 07:38:46 +0700

    Menambahkan Class TeamMembers_1302210070
    Menambahkan Class TeamMembers_1302210070

--- modul7_kelompok_eliteglobal/modul7_kelompok_eliteglobal/Program.cs
+++ modul7_kelompok_eliteglobal/modul7_kelompok_eliteglobal/Program.cs
@@ -2,3 +2,6 @@
 
 DataMahasiswa_1302210070 ashar = new DataMahasiswa_1302210070();
 ashar.ReadJSON();
+
+TeamMembers_1302210070 ashar2 = new TeamMembers_1302210070();
+ashar2.ReadJSON();
---
+++ modul7_kelompok_eliteglobal/modul7_kelompok_eliteglobal/TeamMembers_1302210070.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul7_kelompok_eliteglobal
+{
+    internal class TeamMembers_1302210070
+    {
+        public void ReadJSON()
+        {
+            string jsonString = File.ReadAllText(@"E:\Jurnal_GlobalElite\modul7_kelompok_eliteglobal\modul7_kelompok_eliteglobal\jurnal7_2_1302210070.json");
+            dynamic jsonObject = JsonConvert.DeserializeObject<dynamic>(jsonString);
+
+            Console.WriteLine();
+            Console.WriteLine("Team Member: ");
+            int iter = 1;
+            foreach(var member in jsonObject.members)
+            {
+                Console.WriteLine($"{member.nim}-{member.firstName} {member.lastName}, {member.age}, {member.gender}");
+                iter++;
+            }
+        }
+    }
+}
